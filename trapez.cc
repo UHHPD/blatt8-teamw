b0068401eec025659091e73caba8f0a4a386ec5f
repl.it user <replituser@example.com>
2020-07-05 08:21:28 +0000

    Aufgabe 1

--- trapez.cc
+++ trapez.cc
@@ -8,27 +8,34 @@ public:
   double operator()(double x) { return 3 * x + 2; }
 };
 
+class Pol2 {
+public:
+  double operator()(double x) { return -2* x*x + 3*x + 1; }
+};
+
 class Gauss {
 public:
   double operator()(double x) { return 1 / (sqrt(M_PI * 2)) * exp(-x * x / 2); }
 };
 
 // berechnet Werte nach Trapezformel von I_0 bis I_N
-std::vector<double> trapez(double a, double b, int N) {
-  Pol1 f;
+template <class Functor> std::vector<double> trapez(Functor f, double a, double b, int N) {
   std::vector<double> I(N + 1); // Feld mit N+1 Eintraegen
   const double h = b - a;
   I[0] = h / 2 * (f(a) + f(b));
   for (int k = 1; k <= N; ++k) {
     int n = pow(2, k);
-    //...
-    I[k] = 0; // setze k-ten Wert im Feld
+    double c = 0;
+    for (int i=1; i<n; i+=2) {
+      c += f(a+i*h/n);
+    }
+    I[k] = I[k-1]/2 + h*c/n; // setze k-ten Wert im Feld
   }
   return I;
 }
 
 // berechnet die Richardsonextrapolation aus I(k-1)  und I(k)
-double richardson(double Iprev, double I) { return 0; }
+double richardson(double Iprev, double I) { return I + (I - Iprev) / 3; }
 
 // berechet Naeherungen ueber das Romberg-Verfahren
 // I: Ergebnis von trapez()
@@ -41,13 +48,14 @@ std::vector<std::vector<double>> romberg(std::vector<double> I) {
   return R;
 }
 
-/*
+
 void testeAufgabe1() {
   Pol1 f;
   std::vector<double> I_f = trapez(f, 0, 3, 3);
   for (double tf : I_f) {
     std::cout << "A1: f:" << tf << " == " << 19.5 << ":" << (tf == 19.5 ? "ja" : "nein") << std::endl;
   }
+  
   Pol2 g;
   std::cout << "A1: g(1) = 2 ?" << (g(1) == 2 ? "ja" : "nein") << std::endl;
   std::vector<double> I_g = trapez(g, 0, 3, 3);
@@ -56,8 +64,9 @@ void testeAufgabe1() {
   std::cout << "A1: g2:" << I_g[2] << " == " << -2.0625 << ":" << (I_g[2] == -2.0625 ? "ja" : "nein") << std::endl;
   double rich = richardson(I_g[0], I_g[1]);
   std::cout << "A1: Richardson : " << rich << " : " << (rich == -1.5 ? "ja " : "nein") << std::endl;
-}
 
+}
+/*
 void testeAufgabe2() {
   Pol1 f;
   std::vector<std::vector<double>> Rf = romberg(trapez(f, 0, 3, 3));
@@ -82,7 +91,7 @@ int main() {
   Pol1 f;
   std::cout << "f(0) = " << f(0) << '\n';
   // berechne Trapezformel fuer f
-  std::vector<double> tf = trapez(0., 3., 3);
+  std::vector<double> tf = trapez(f, 0., 3., 3);
   std::cout
       << "#############################################################\n";
   // Ausgabe:
@@ -98,8 +107,8 @@ int main() {
     }
     std::cout << std::endl;
   }
-  /*
+
   testeAufgabe1();
-  testeAufgabe2();
-  */
+  // testeAufgabe2();
+
 }
